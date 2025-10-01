8e5e97447faab4ce48eb2d89ade1fc988aabe435
shivani-backflipt <shivanicherukuru@backflipt.com>
2025-10-01 16:15:01 +0530

    adding number guess program

--- number_guess.py
+++ number_guess.py
@@ -1,16 +1,17 @@
 import random
 
 random_number= random.randint(1,100)
-guess = input()
+guess=None
 
-print ("Guess the number between 1 to 100")
-
-
-
-if guess > random_number:
-    print("decrease the number")
-elif guess < random_number:
-    print ("increase the number")
-else:
-    print ("the number is:",random_number)
+while guess!=random_number:
+    print ("enter the number you guess")
+    guess = int (input())
+    if guess > 100 or guess < 1:
+        print("please enter number between 1 and 100")
+    elif guess > random_number:
+        print("decrease the number")
+    elif guess < random_number:
+        print ("increase the number")
+    else:
+        print ("Congratulations! you guess is right, the number is",random_number)
 
