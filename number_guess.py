133a64bb12ea58374776d74c5bb5eb69355d6db9
shivani-backflipt <shivanicherukuru@backflipt.com>
2025-10-01 14:41:10 +0530

    adding notes folder

--- number_guess.py
+++ number_guess.py
@@ -1,2 +1,16 @@
-print ("hello")
-print ("hh")
+import random
+
+random_number= random.randint(1,100)
+guess = input()
+
+print ("Guess the number between 1 to 100")
+
+
+
+if guess > random_number:
+    print("decrease the number")
+elif guess < random_number:
+    print ("increase the number")
+else:
+    print ("the number is:",random_number)
+
